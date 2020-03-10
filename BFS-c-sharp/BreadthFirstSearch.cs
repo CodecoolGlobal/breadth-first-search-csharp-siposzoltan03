6d2e9f538cd42ac0a7e12d0be91e6a712834afde
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-03-10 22:05:46 +0100

    Implement getFriendsOfFriends method

--- BFS-c-sharp/BreadthFirstSearch.cs
+++ BFS-c-sharp/BreadthFirstSearch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using BFS_c_sharp.Model;
 
 namespace BFS_c_sharp
@@ -26,10 +27,7 @@ namespace BFS_c_sharp
                 Queue<FriendDistance> queue = new Queue<FriendDistance>();
                 HashSet<UserNode> checkedFriends = new HashSet<UserNode>();
 
-                foreach (UserNode friend in userNode1.Friends)
-                {
-                    AddFriendToQueue(queue, friend);
-                }
+                SeedQueueWithRootUserFriends(userNode1, queue);
 
                 while (queue.Count > 0)
                 {
@@ -40,6 +38,7 @@ namespace BFS_c_sharp
                         {
                             return friendDistance.Distance + 1;
                         }
+
                         if (!checkedFriends.Contains(friend))
                         {
                             AddFriendToQueue(queue, friend);
@@ -57,16 +56,62 @@ namespace BFS_c_sharp
             return 0;
         }
 
-        
-        
-        
-        
-        private static void AddFriendToQueue(Queue<FriendDistance> queue, UserNode userNodeFriend)
+        private static void SeedQueueWithRootUserFriends(UserNode user, Queue<FriendDistance> queue)
+        {
+            foreach (UserNode friend in user.Friends)
+            {
+                AddFriendToQueue(queue, friend);
+            }
+        }
+
+        public static List<UserNode> GetFriendsOfFriends(UserNode user, int depth)
+        {
+            if (depth < 1)
+            {
+                throw new ArgumentOutOfRangeException($"Depth must be at least 1!");
+            }
+
+            int currentDepth = 1;
+            HashSet<UserNode> friends = new HashSet<UserNode>();
+            Queue<FriendDistance> watchableFriends = new Queue<FriendDistance>();
+            HashSet<UserNode> watchedFriends = new HashSet<UserNode>();
+
+            foreach (UserNode friend in user.Friends)
+            {
+                friends.Add(friend);
+                watchedFriends.Add(friend);
+                if (depth > currentDepth)
+                {
+                    AddFriendToQueue(watchableFriends, friend, currentDepth);
+                }
+            }
+
+            while (watchableFriends.Count > 0)
+            {
+                var friendDistance = watchableFriends.Dequeue();
+                friends.Add(friendDistance.UserNode);
+                if (friendDistance.Distance < depth)
+                {
+                    foreach (UserNode friend in friendDistance.UserNode.Friends)
+                    {
+                        if (!watchedFriends.Contains(friend))
+                        {
+                            AddFriendToQueue(watchableFriends, friend, currentDepth + 1);
+                            watchedFriends.Add(friend);
+                        }
+                    }
+                }
+            }
+            return friends.ToList();
+        }
+
+
+        private static void AddFriendToQueue(Queue<FriendDistance> queue, UserNode userNodeFriend, int distance = 1)
         {
             queue.Enqueue(new FriendDistance
             {
                 UserNode = userNodeFriend,
-                Distance = 1
+                Distance = distance
             });
         }
     }
--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -11,13 +11,27 @@ namespace BFS_c_sharp
             RandomDataGenerator generator = new RandomDataGenerator();
             List<UserNode> users = generator.Generate();
 
-            Console.WriteLine("" + users[0] 
-                                 + users[0] + "Distance is:" 
-                                 + BreadthFirstSearch.GetMinimumDistance(users[0], users[0]));
-            Console.WriteLine("" + users[1] 
-                                 + users[28] + "Distance is:" 
-                                 + BreadthFirstSearch.GetMinimumDistance(users[1], users[28]));
-            
+            // Console.WriteLine("" + users[0] 
+            //                      + users[0] + "Distance is:" 
+            //                      + BreadthFirstSearch.GetMinimumDistance(users[0], users[0]));
+            // Console.WriteLine("" + users[1] 
+            //                      + users[28] + "Distance is:" 
+            //                      + BreadthFirstSearch.GetMinimumDistance(users[1], users[28]));
+
+            List<UserNode> friendsOfFriendsDepth1 = BreadthFirstSearch.GetFriendsOfFriends(users[0], 1);
+            foreach (var friend in friendsOfFriendsDepth1)
+            {
+                Console.WriteLine(friend);
+            }
+
+            Console.WriteLine("<--------------------------------------------------->");
+            List<UserNode> friendsOfFriendsDepth2 = BreadthFirstSearch.GetFriendsOfFriends(users[0], 2);
+            foreach (var friend in friendsOfFriendsDepth2)
+            {
+                Console.WriteLine(friend);
+            }
+
+
             // foreach (var user in users)
             // {
             //     Console.WriteLine(user);
